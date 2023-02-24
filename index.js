cff856c11337e6142b670bc319a11c393680882b
krzxa <100853430+krzxa@users.noreply.github.com>
2023-02-24 23:18:55 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { token } = require('./config.json');
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(token);
